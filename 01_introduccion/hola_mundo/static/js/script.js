98c343c3ef727c073a3bf982b0d6ca5ef9f20b2a
alansepulveda-creator <alansepulveda@liceovvh.cl>
2026-04-15 08:10:11 -0400

    ok

---
+++ 01_introduccion/hola_mundo/static/js/script.js
@@ -0,0 +1,13 @@
+console.log("hola mundo, desde archivo externo...");
+// prueba exitosa cun js
+console.log("Conexion exitosa con js externo...");
+// 1) Hola Mundo con variable y const.
+const saludo = "Hola a todos, ";
+let destinatario = "estudiantes";
+// impresion de saludo concatenado con (+)
+console.log(saludo + "para los " + destinatario)
+
+// 2) saludar a una persona ingresada por teclado.
+let nombre = prompt("Ingrese su nombre: ");
+//Imprimir un nombre ingresado por un  prompt
+console.log("Hola " + nombre)
---
+++ 02_tipos_de_datos/1-tipo_texto/static/js/script.js
@@ -0,0 +1,9 @@
+console.log("conexion con js correcta...");
+/*
+concatenación con el signo +
+podemos unir texto y variables
+*/
+const nombre = "Alan";
+const apellido = "Sepúlveda";
+//unimos ambas constantes con un texto extra
+console.log("Hola, mi Nombre es: " + nombre + " " + apellido);
